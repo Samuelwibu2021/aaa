4a9e48551f875e95691b3edb3a218c07d77172e5
Samuel <samueloliver760@gmail.com>
2021-01-02 22:56:01 +0700

    Initial commit

---
+++ src/main/java/com/wibu/linechatbotdicoding/LinechatbotdicodingApplication.java
@@ -0,0 +1,13 @@
+package com.wibu.linechatbotdicoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinechatbotdicodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinechatbotdicodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wibu/linechatbotdicoding/LinechatbotdicodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wibu.linechatbotdicoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinechatbotdicodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
